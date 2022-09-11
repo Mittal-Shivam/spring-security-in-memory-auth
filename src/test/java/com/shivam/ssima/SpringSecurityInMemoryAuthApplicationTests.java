60cd0bc39be08da441e200b514c4567908e0a126
Mittal-Shivam <shivam00035@gmail.com>
2022-09-11 23:47:50 +0530

    Initial Commit

---
+++ src/test/java/com/shivam/ssima/SpringSecurityInMemoryAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.ssima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInMemoryAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
